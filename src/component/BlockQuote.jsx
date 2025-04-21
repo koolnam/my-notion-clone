ddbb95a0cb1b78a8b431167ec9b063f23de5a762
Yuvraj Chauhan <yuvrajchauhan2060@gmail.com>
2025-04-21 11:02:39 +0545

    chore: align middle

--- src/component/BlockQuote.jsx
+++ src/component/BlockQuote.jsx
@@ -2,11 +2,11 @@ import pin from "../assets/pin.png"
 export function BlockQuote() {
     return(
       <section className="flex justify-center items-center w-full max-w-[1252px] m-auto py-16">
-      <div className="relative max-w-none w-[80%] sm:w-[60%] md:w-auto transform rotate-[-3deg]">
-        <div className="absolute w-[64px] h-full top-[-40px] lg:top-[-50px] left-1/2 -translate-x-1/2 z-10">
+      <div className="max-w-none w-[80%] sm:w-[60%] md:w-auto transform rotate-[-3deg]">
+        <div className="relative bg-[#f8f7f4] px-6 py-8 lg:px-20 lg:py-16 lg:pt-14 shadow-md">
+        <div className="absolute w-[64px] h-full top-[-40px] lg:top-[-65px] left-[calc(50%+24px)] -translate-x-1/2 z-10">
           <img src={pin} alt="" className="w-[40px] lg:w-[80px] object-contain" />
         </div>
-        <div className="relative bg-[#f8f7f4] px-6 py-8 lg:px-20 lg:py-16 lg:pt-14 shadow-md">
           <blockquote className="text-center">
             <p className="text-xl md:text-3xl lg:text-[42px] font-serif mb-4 whitespace-nowrap">
               "Your AI everything app."
